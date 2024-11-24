636d2e7d6b15d9972ea6e57f18be7515f1959317
Omkar-t06 <143606533+Omkar-t06@users.noreply.github.com>
2024-11-24 15:55:37 +0530

    step - 3

---
+++ src/app/middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from 'next-auth/jwt';
+export { default } from 'next-auth/middleware';
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({req});
+    const url = req.nextUrl;
+
+    if(token && 
+        (
+            url.pathname.startsWith('/sign-in') || 
+            url.pathname.startsWith('/sign-up') ||
+            url.pathname.startsWith('/verify') ||
+            url.pathname.startsWith('/')
+        )
+    ) {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+    }
+
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/sign-in', req.url));
+    }
+    
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/sign-in',
+        '/sign-up',
+        '/',
+        '/dashboard/:path*',
+        '/verify/:path*',
+    ],
+
+}
